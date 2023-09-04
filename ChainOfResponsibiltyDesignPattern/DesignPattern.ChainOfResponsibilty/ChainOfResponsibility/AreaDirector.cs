cf1a2ad8c06837b2786f964a21cac904ccf1314c
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-09-04 16:36:42 +0300

    Migrated to SQL Server Created 4(Treasurer - Manager Asistant - Manager - Area Director) classes who uses inheritence from Employee Class.

--- ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/AreaDirector.cs
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/AreaDirector.cs
@@ -3,31 +3,31 @@ using DesignPattern.ChainOfResponsibilty.Models;
 
 namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibility
 {
-    public class Manager : Employee
+    public class AreaDirector : Employee
     {
         public override void ProcessRequest(CustomerProcessViewModel req)
         {
             Context context = new Context();
-            if (req.Amount <= 250000)
+            if (req.Amount <= 400000)
             {
                 CustomerProcess customerProcess = new CustomerProcess();
                 customerProcess.Amount = req.Amount.ToString();
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName = "Şube Müdürü - Hatice Sarı";
+                customerProcess.EmployeeName = "Bölge Müdürü - Yasin Yağcı";
                 customerProcess.Description = "Para Çekme İşlemi Onaylandı , Müşteriye Talep Ettiği Tutar Ödendi";
                 context.customerProcesses.Add(customerProcess);
                 context.SaveChanges();
             }
-            else if (NextApprover != null)
+            else
             {
                 CustomerProcess customerProcess = new CustomerProcess();
                 customerProcess.Amount = req.Amount.ToString();
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName = "Şube Müdürü - Hatice Sarı";
-                customerProcess.Description = "Para Çekme Tutarı Şube Müdürünün  Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Bölge Müdürüne Yönlendirildi.";
+                customerProcess.EmployeeName = "Bölge Müdürü - Yasin Yağcıı";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Müdürünün  Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi, Müşterinini Günlük Maksimum Çekebileceği Tutar 400.000 ₺ Olup Daha FAzlası İçin Birden Fazla Gün Şubeye Gelmesi Gerekli...";
                 context.customerProcesses.Add(customerProcess);
                 context.SaveChanges();
-                NextApprover.ProcessRequest(req);
+                
             }
         }
     }
